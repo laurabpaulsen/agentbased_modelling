4fcab4823ac5e11af167f2dac834250b54cbd645
Laura Paulsen <202005791@post.au.dk>
2022-04-21 22:19:45 +0200

    update

--- ABM_functions.py
+++ ABM_functions.py
@@ -3,19 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 ########## PREPERATION ##########
 # defining agents as abstact class
 class Agent:
     # init-method, the constructor method for agents
     # maybe we dont need the position and index parameters? these will be given in the populate_company function
-    def __init__(self, position, index, gender, age, senority, fire):
+    def __init__(self, position, index, gender, age, seniority, fire, seniority_position):
         self.position = position
         self.index = index
         self.gender = gender
         self.age = age
-        self.senority = senority
-        self.fire = fire[0]
+        self.seniority = seniority
+        self.fire = fire[0] # could potentially be dependent on the sex and the seniority and the position?
+        self.seniority_position = seniority_position
 
 
 # function for creating empty dictionary (company)
@@ -49,7 +49,7 @@ def populate_company(company: dict, weights: dict):
     '''
     for i in company.keys():
         for j in range(0, len(company[i])):
-            company[i][j] = Agent(position = i, index = j, gender = random.choices(['male', 'female'], weights = weights[i]['weights'], k = 1), age = weights[i]['age'], senority = weights[i]['senority'], fire = random.choices([True, False], weights = weights[i]['fire']))
+            company[i][j] = Agent(position = i, index = j, gender = random.choices(['male', 'female'], weights = weights[i]['weights'], k = 1), age = random.gauss(weights[i]['age'][0], weights[i]['age'][1]), seniority = random.gauss(weights[i]['seniority'][0], weights[i]['seniority'][1]), fire = random.choices([True, False], weights = weights[i]['fire']), seniority_position = random.gauss(weights[i]['seniority_position'][0], weights[i]['seniority_position'][1]))
 
 
 # function for counting gender of agents in the different hierchical levels of the company
@@ -121,7 +121,7 @@ def plot_gender_development(company: dict, months:int, data: pd.DataFrame):
     months: int, the number of months simulated
     data: pandas dataframe, a dataframe with the information about the genders of employees in each job-title
     '''
-    fig, axs = plt.subplots(nrows = 1, ncols = 4, figsize=(12,5))
+    fig, axs = plt.subplots(nrows = 2, ncols = 3, figsize=(12,5), sharey=False)
     colorlist = {'female': 'skyblue', 'male': 'steelblue'}
     markers = [plt.Line2D([0,0],[0,0],color=color, marker='o', linestyle='') for color in colorlist.values()]
     plt.legend(markers, colorlist.keys(), numpoints=1) # legend
@@ -159,8 +159,31 @@ def get_nth_key(dictionary: dict, n=0):
             return key
     raise IndexError("dictionary index out of range")
 
+def normalize_weights(list):
+    '''
+    Normalizes a list between 0 and 1
+    '''
+    try:
+        list = [float((i - min_none(list))) / float((max_none(list) - min_none(list))) if i != None else 0 for i in list]
+    except(ZeroDivisionError):
+        list = None # if the denominator is 0 the weights are equal
+    return list
 
-def promote_agent(company:dict, i, j, ind_i, weight:dict):
+def min_none(list):
+    '''
+    Returns the minimum value of a list, ignores None values
+    '''
+    newlist = min([value for value in list if value != None])
+    return newlist
+
+def max_none(list):
+    '''
+    Returns the maximum value of a list, ignores None values
+    '''
+    newlist = max([value for value in list if value is not None])
+    return newlist
+
+def promote_agent(company:dict, i, j, ind_i, weight:dict, bias:float):
     '''
     Promotes an agent if the empty position is not at the lowest level of the company. It the empty position is at the lowest level of the company, a new agent is generated and hired. 
 
@@ -171,23 +194,38 @@ def promote_agent(company:dict, i, j, ind_i, weight:dict):
     j : the index at which a position is available at level i
     ind_i : the index of the level at which a position is available
     weight : dictionary, a dictionary containing the information used to generate agents (used to generate new agents at he lowest level)
+    bias : float, a bias added to all males to either increase or decrease their chance of getting promoted
     '''
     # if level is the lowest, a new agent is created
     if i == list(company.keys())[-1]:
-        company[i][j] = Agent(position = i, index = j, gender = random.choices(['male', 'female'], weights = weight[i]['weights'], k = 1), age = weight[i]['age'], senority = weight[i]['senority'], fire = weight[i]['fire'])
+        company[i][j] = Agent(position = i, index = j, gender = random.choices(['male', 'female'], weights = weight[i]['weights'], k = 1), age = random.gauss(weight[i]['age'][0], weight[i]['age'][0]), seniority = random.gauss(weight[i]['seniority'][0], weight[i]['seniority'][1]), fire = weight[i]['fire'], seniority_position = 0) # SHOULD WE PUT IN A RANDOM ONE MAYBE THEY HAD A SIMILAR POSITION
     
     elif i != list(company.keys())[-1]:
         weights = []
         # determining weight from the attributes of agents on the level lower that the empty spot
-        # the weight is determined by the age and the senority of the agent and normalized by the number of agents on the level lower that the empty spot
+        # the weight is determined by the age and the seniority of the agent and normalized by the number of agents on the level lower that the empty spot
         index = get_nth_key(company, (ind_i+1))
         for k in range(0, len(company[index])):
             if company[index][k] is not None:
-                weights.append((company[index][k].age + company[index][k].senority)/(len(company[index])))
+                if company[index][k].gender[0] == 'male':
+                    add_bias = bias
+                else:
+                    add_bias = 0
+                weights.append((company[index][k].age + company[index][k].seniority)/(len(company[index])) + add_bias)
             else:
-                weights.append(0)
-        # the agent with the highest weight is promoted (could be changed to random choice?)
-        agent = company[index][np.argmax(weights)]
+                weights.append(None)
+        #np.argsort(test_list, axis=-1)[-candidates:] -weight[index]['candidates']
+        promotion_candidates_index = np.argsort(np.where(~np.isin(weights, [None]), weights, -1e10))[-weight[i]['candidates']:]
+
+        #normalizing weights THIS TAKES A LONG TIME... how can we speed this up?
+        weights = np.array(weights)[promotion_candidates_index]
+        weights = normalize_weights(weights)
+
+        # an agent from the promotion candidates is promoted using the random choice function, with weights calculated - CHECK THAT THIS WORKS AS INTENDED
+        agent = company[index][random.choices(promotion_candidates_index, weights = weights)[0]]
+
+        # setting the seniority in the position as 0
+        agent.seniority_position = 0
 
         # promoting the agent
         company[i][j] = agent
@@ -195,7 +233,7 @@ def promote_agent(company:dict, i, j, ind_i, weight:dict):
         company[agent.position][agent.index] = None
         # changing the position of the agent
         company[i][j].postition = i
-            # changing the index of the agent
+        # changing the index of the agent
         company[i][j].index = j
 
 
@@ -210,7 +248,7 @@ def fire_agent(company, i, j):
 
 def update_agents(company, i, j, weight:dict):
     '''
-    Updates the agents each tick (e.g. adding a month to senority and age)
+    Updates the agents each tick (e.g. adding a month to seniority and age)
 
     Parameters
     ----------
@@ -219,8 +257,10 @@ def update_agents(company, i, j, weight:dict):
     j : the index of the agent at the job-title
     weight : dictionary, a dictionary containing the information used to generate agents
     '''
-    # adding a month to the senority of all agents
-    company[i][j].senority += 1/12
+    # adding a month to the seniority of all agents
+    company[i][j].seniority += 1/12
+    # adding a month to the position seniority of all agents
+    company[i][j].seniority_position += 1/12
     # adding a month to the age of all agents
     company[i][j].age += 1/12
     company[i][j].fire = random.choices([True, False], weights = weight[i]['fire'], k = 1)
@@ -236,7 +276,7 @@ def update_agents(company, i, j, weight:dict):
 # check savepath in beginning of simulation
 # figure out what data to include
 
-def run_abm(months: int, save_path: str, company_titles: list, titles_n: list, weights: dict, plot_each_tick = False):
+def run_abm(months: int, save_path: str, company_titles: list, titles_n: list, weights: dict, bias: float, plot_each_tick = False):
     '''
     Runs the ABM simulation
 
@@ -248,6 +288,7 @@ def run_abm(months: int, save_path: str, company_titles: list, titles_n: list, w
     titles_n : list, a list of integers with the number of agents in each job title
     plot_each_tick : bool, if True, the gender distribution is plotted after each tick
     weights : dictionary, a dictionary containing the information used to generate agents
+    bias : float, a bias for the company's hiring process (positive bias = males are more likely to be promoted)
     '''
     # creating empty dataframe for the results
     col_names = ['tick', 'gender']
@@ -273,14 +314,14 @@ def run_abm(months: int, save_path: str, company_titles: list, titles_n: list, w
                     fire_agent(company, i, j)
 
                 if company[i][j] == None:
-                    promote_agent(company, i, j, ind_i, weight=weights)
+                    promote_agent(company, i, j, ind_i, weight=weights, bias=bias)
             
         # plotting and appending data to data frame                           
         if plot_each_tick:
             plot_gender(company, tick = month)
 
-        f = {'gender': 'female', 'tick': month, 'Department Head': count_gender(company)[0][0],'Leader': count_gender(company)[0][1], 'Senior': count_gender(company)[0][2], 'Junior': count_gender(company)[0][3]}
-        m = {'gender': 'male', 'tick': month, 'Department Head': count_gender(company)[1][0],'Leader': count_gender(company)[1][1], 'Senior': count_gender(company)[1][2], 'Junior': count_gender(company)[1][3]}
+        f = {'gender': 'female', 'tick': month, 'C-Suite': count_gender(company)[0][0],'SVP': count_gender(company)[0][1], 'VP': count_gender(company)[0][2], 'Senior Manager': count_gender(company)[0][3], 'Manager': count_gender(company)[0][4], 'Entry Level': count_gender(company)[0][5]}
+        m = {'gender': 'male', 'tick': month, 'C-Suite': count_gender(company)[1][0],'SVP': count_gender(company)[1][1], 'VP': count_gender(company)[1][2], 'Senior Manager': count_gender(company)[1][3], 'Manager': count_gender(company)[1][4], 'Entry Level': count_gender(company)[1][5] }
         
         # create pandas dataframe from dictionaries f and m
         new_data = pd.DataFrame.from_dict([f, m])
@@ -289,7 +330,7 @@ def run_abm(months: int, save_path: str, company_titles: list, titles_n: list, w
         print('tick {} done'.format(month))
 
     # plotting the gender development over time
-    plot_gender_development(company, months=months, data = data)
+    plot_gender_development(company, months = months, data = data)
     # saving the data to a csv-file
     data.to_csv(save_path)
 
--- README.md
+++ README.md
@@ -6,13 +6,51 @@ Investigating recruitment
     * Does gender modulate the chance of getting promoted?
     * Is it modulated by who filled the position before?
 
+* Kan det mere skiftende arbejdsmarked blive en fordel for kvinder?
 
-Instead of simulating a company we could simulate the entire 'office-world' in Denmark? 
+
+
+
+Should the company be assumed to be growing, creating vacancies within each rank
+"Vacancies at the entry level are filled through hiring from a hypothetical external pool of candidates, while vacancies at all other ranks are filled by promoting individuals from the rank immediately below, which, in turn, creates additional vacancies at the rank from which someone was promoted." - An Agent-Based Simulation of How Promotion Biases Impact Corporate Gender Diversity
+* This is partially what is done now, except for the fact that a random agent is generated at the entry level
+"For this version of the simulation we do not simulate direct hiring into higher ranks, nor do we allow promotions to skip levels. However, these and other details could be added if we were interested in exploring the effects of such modifications."
+* We could potentially do this?
+
+"By default, the promotion pool is set as a percentage of the total number of employees at that rank, and is based on the “promotion score” of each employee, a normalized value based on the amount of time the employee has spent at the current rank (“rank seniority”), relative to the amount of time spent by the most senior employee at that rank. In other words, in the absence of a bias, promotions are based on rank seniority, with some randomness to reflect the fact that seniority is not an exact indicator of merit."
+* maybe clever enough to have a pool - we could test the implications of the shortlist article this way?
+
+
+"When an employee is promoted, its seniority at the new rank is set to zero, while the simulation still tracks the total amount of time that the employee has been with the organization."
+* This should potentially be done?
+
+"Gender biases in the promotion process are simulated by adding a promotion-bias parameter, a value that is added to the rank seniority of each employee to calculate the promotion scores. The promotion-bias parameter can be positive or negative to simulate biases that favor men or women, respectively."
 
 
 
 
 
 ### Code to-do
-[] What kind of data do we need to save as we go?
-[X] Give weights in populate_company function rather than if statements in the function
+
+    [] What kind of data do we need to save as we go?
+    [X] Give weights in populate_company function rather than if statements in the function
+    [] Should senority be 0 for randomly generated new agents? 
+    [] How many employees
+        "For all results reported here, the company begins with a total of 300 employees, 150 women and 150 men. The employees are distributed across the four levels in a way that roughly simulates a typical company: 40% at the entry level, 30% at the manager level, 20% at the VP level and 10% at the executive level"
+    [] Annual churn rate
+
+
+
+
+
+
+
+
+###
+
+* forskellige tiltag hvor lang tid tager det?
+* virksomhed, så mange parametre som muligt
+    * kør med forskellige bias (+ til mændene, + til kvinderne, 0 dvs ingen forskel)
+* flere kvinder på højere niveau, større sandsynlighed for at de søger
+* med bias omkring at næste hire skal ligne den tidligere, og så fjerner man det
+
